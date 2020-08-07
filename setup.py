266198b3e06a98f2d1254badeaa3b365203431ff
Northguard <northguard_serve@tutanota.com>
2020-08-07 14:28:52 +0200

    Link from PyPI to GitHub.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from figure_manager import __version__
 long_description = """
 `figure_manager` can snap matplotlib-figures to various positions on the screen.  
 For example, more one figure to upper-left corner of the screen or make a figure full-screen. 
+
+More information at https://github.com/NorthGuard/figure_manager
 """
 
 setup(
