487ec41b7d1621045d3299671ed0769c0116b2c7
Northguard <northguard_serve@tutanota.com>
2021-05-28 11:56:04 +0200

    Version 0.1.4
    Fixed bad import reference.

--- figure_manager/__init__.py
+++ figure_manager/__init__.py
@@ -1,5 +1,5 @@
-from figure_manager.figure_manager import FigureManager
-from figure_manager.figure_manager import get_figure_manager
+from .figure_manager import FigureManager
+from .figure_manager import get_figure_manager
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Jeppe Nørregaard"
